a7d47dfaece86a6bc52e125b30f22350a99b2ce2
Qquanwei <quanwei9958@126.com>
2017-08-14 12:07:42 +0800

    dump 0.0.7

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import { minify } from 'uglify-es'
-import uglify from 'rollup-plugin-uglify'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 
@@ -9,7 +7,6 @@ export default {
     dest: 'lib/bundle.js',
     sourceMap: true,
     plugins: [
-        uglify({}, minify),
         resolve({
             jsnext: true,
             main: true,
