0efefd8bb6218df2cf420f4bc8f217a3d4a2d424
Qquanwei <quanwei9958@126.com>
2017-08-14 12:16:30 +0800

    dump 0.0.8
    support es6 -> es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import buble from 'rollup-plugin-buble'
 
 export default {
     entry: './index.js',
@@ -7,6 +8,7 @@ export default {
     dest: 'lib/bundle.js',
     sourceMap: true,
     plugins: [
+	buble(),
         resolve({
             jsnext: true,
             main: true,
